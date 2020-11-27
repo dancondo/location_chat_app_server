c2d643bfd8edf020aacb015e6e96a16f1212a538
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-11-26 22:27:37 -0300

    deps

--- mix.exs
+++ mix.exs
@@ -37,9 +37,11 @@ defmodule LocationChatApp.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
+      {:ecto_enum, "~> 1.4"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:geo_postgis, "~> 3.1"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
