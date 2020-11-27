3262c3c0acd3a62b8c62b4e2619d66b9bee6a7a2
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-11-27 14:19:30 -0300

    user controller

---
+++ lib/location_chat_app_web/controllers/user_controller.ex
@@ -0,0 +1,29 @@
+defmodule LocationChatAppWeb.UserController do
+  use LocationChatAppWeb, :controller
+
+  alias LocationChatApp.Accounts
+  alias LocationChatApp.Accounts.User
+
+  action_fallback LocationChatAppWeb.FallbackController
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    render(conn, "show.json", user: user)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+
+    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+      render(conn, "show.json", user: user)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+
+    with {:ok, %User{}} <- Accounts.delete_user(user) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
